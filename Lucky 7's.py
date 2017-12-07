5d8426982f91f393faa4a00e72529d0c182881b6
757u <31043648+757u@users.noreply.github.com>
2017-12-07 09:46:24 -0800

    Added turns_left -=
    12/7/17

---
+++ Lucky 7's.py
@@ -0,0 +1,5 @@
+# You are making a gambling game that demonstrates how much money can be lost. The game is rigged and you are guaranteed to lose all of your (fictional) money at some point.
+
+# You start with $15. Each time you play (round), you make a bet of $1. Two dice (D6) are rolled. If the sum of the numbers is the number 7, then you win your bet back, plus an additional $4.
+
+# When the game is run, it will keep running until you have run out of money. The program will then display how many rounds it took to run out of money.
--- Warm ups.py
+++ Warm ups.py
@@ -2,24 +2,34 @@
 
 # 12.4.17
 
-def reverse_order(first_name, last_name):
-    print("%s, %s," % (last_name, first_name))
-
-
-
-reverse_order("Jazmin", "Ambriz")
-
+#     print("%s, %s," % (last_name, first_name))
+#
+#
+#
+# reverse_order("Jazmin", "Ambriz")
 
 # 12.5.17
 
-def add_py(py):
-    print("%s.py" % name)
+# def add_py(py):
+#     print("%s.py" % name)
 
-
+#
 # 12.6.17
 
-def add(num_1, num_2, num_3):
-    print(num_1 + num_2 + num_3)
+# def add(num_1, num_2, num_3):
+#     print(num_1 + num_2 + num_3)
 
-add(15, 18, 9000)
-add(1, 2, 3)
+# add(15, 18, 9000)
+# add(1, 2, 3)
+# 12.7.17
+
+def repeat(string):
+    print(string)
+    print(string)
+    print(string)
+
+repeat("string")
+
+for x in range(3):
+    print("string")
+
