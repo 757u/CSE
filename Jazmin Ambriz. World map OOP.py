ce630f182b6b04503b707264aee89266d6fc725a
757u <31043648+757u@users.noreply.github.com>
2018-02-28 09:46:24 -0800

    Added Rooms
    02/28/18

--- Jazmin Ambriz. World map OOP.py
+++ Jazmin Ambriz. World map OOP.py
@@ -17,8 +17,28 @@ class Room(object):
 
 
 living = Room("Living Room", None, None, None, 'kitchen', None, None, None, None, 'There are three couches and a TV.')
+
 kitchen = Room("Kitchen", "Old Room", None, "Living", "Garden", None, None, None, None, "Big wooden table, kitchen has"
-                "old rusty drawers with a sink. To the west there is a big clear door leading to the garden")
+               "old rusty drawers with a sink. To the west there is a big clear door leading to the garden")
+
+Garden = Room("Garden", None,None, "Antique Art Room", None, None, None, None, None, "'Green grass all over, colorful"
+             " flower are starting to bloom")
+Art_Room = Room("Art Room", None, None, None, "Garden", None, None, None, None, "To the east there is a brick wall,"
+               " which has no exist. In the middle of the room there is an art stand with some dry paints by the side.")
+
+Southeast_of_Garden = Room("Front of House", "Inside of House", "Dangerous Forest", None, None, None, None, None, None,
+                           "Black two story house, with two white windows in front.")
+
+Inside_House = Room("Inside House", "Inside Pool", None, "Living Room", None, None, None, "Bedroom", "Kitchen", "You"
+                    "are inside a two story house; standing at the beginning of a corridor. Around you there are"
+                    "different rooms and doors leading to the yet unknown. To the side a bit to the east there"
+                    "is a fancy staircase leading to the second floor.")
+
+Swimming_pool = Room("Inside Pool", None, None, "Big Bathroom", None, "Bedroom", None, None, None, "Big rectangular pool"
+                    "with crystal water, the bottom can not be seen because of its deepness, inside the pool there are"
+                    "infinite steps leading down to the bottom of the pool (if there is a bottom.")
+
+
 
 
 
